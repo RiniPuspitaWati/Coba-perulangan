1e4d44ccec9756393d9e77dbee46f4d49cea1415
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-09 19:41:44 +0700

    Add files via upload

---
+++ Coba perulangan.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	for(int i=10 ; i>0 ; i--){
+		for(int u=0 ; u<i ; u++){
+			cout<<" * ";
+		}
+		cout<<endl;
+	}
+	
+	//while(true){
+	//	cout<<"Mutiara Cintia Rainy"<<endl;
+	//}
+
+	
+	return 0;
+}
